fcbcd215b2874172c934a40f9951d4034c2bff28
Vlad Grigore <vladyg1986@gmail.com>
2021-02-10 18:24:49 +0200

    validate method: check duplicate and range

--- src/com/company/service/ValidatorService.java
+++ src/com/company/service/ValidatorService.java
@@ -2,7 +2,7 @@ package com.company.service;
 
 public class ValidatorService {
 
-    private IOService ioService;
+    private final IOService ioService;
 
     public ValidatorService(IOService ioService) {
         this.ioService = ioService;
@@ -10,22 +10,22 @@ public class ValidatorService {
 
     public int[] processUserOption(int[] userOption) {
         boolean foundDuplicateIntoArray = foundDuplicateIntoArray(userOption);
-        if (!foundDuplicateIntoArray) {
-            ioService.displayInfo("good job, no duplicate!");
-            for(int i = 0; i < userOption.length; i++) {
-                int curentElement = userOption[i];
-                if (!(curentElement >= 1 && curentElement <= 49)) {
-                    ioService.displayInfo("Invalid number! Out of range 1 - 49!");
-                    userOption = ioService.getUserOption();
-                    return processUserOption(userOption);
-                }
-            }
+        boolean outOfRange = checkOutOfRange(userOption);
+        if (!foundDuplicateIntoArray && !outOfRange) {
+            return userOption;
         } else {
-            System.out.println("you got here now!!!!");
             userOption = ioService.getUserOption();
             return processUserOption(userOption);
         }
-        return userOption;
+    }
+
+    public boolean checkOutOfRange(int[] userOption) {
+        for(int currentElement : userOption) {
+            if (!(currentElement >= 1 && currentElement <= 49)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean foundDuplicateIntoArray(int[] userOption) {
@@ -34,7 +34,6 @@ public class ValidatorService {
                 int i0 = userOption[index0];
                 int i1 = userOption[index1];
                 if (i0 == i1) {
-                    ioService.displayError("Duplicates were found!");
                     return true;
                 }
             }
